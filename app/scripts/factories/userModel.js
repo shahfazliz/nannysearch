1f6061ff8c7c29f1cea5f362b50359fc9185198c
Izzad Halimi <fretwiz@gmail.com>
2015-08-01 00:27:57 +0800

    Create user now send sms to registered number twillio only, need to buy a proper account

--- app/scripts/factories/userModel.js
+++ app/scripts/factories/userModel.js
@@ -9,13 +9,21 @@ angular.module('nannyApp').factory('userModel',['$http', function($http){
 
 		sendTwillioSms : function(to, msg){
 			$http({
-		        method:     'POST',
+				method:     'POST',
 		        url:        'https://api.twilio.com/2010-04-01/Accounts/AC6659e644be085e375752a51422018eef/Messages.json',
-		        headers:{   'Authorization': 'Basic ' + btoa('AC6659e644be085e375752a51422018eef:7f0d9b9ddaa6ee68318efee2f4a229d2')},
+		        headers:{   'Authorization': 'Basic ' + btoa('AC6659e644be085e375752a51422018eef:7f0d9b9ddaa6ee68318efee2f4a229d2'),
+		        			'Content-Type': 'application/x-www-form-urlencoded'
+		    	},
+		        transformRequest: function(obj) {
+			        var str = [];
+			        for(var p in obj)
+			        str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+			        return str.join("&");
+			    },
 		        data: {
-		            'To':     to,
+		            'To':     '+60163064256',
 		            'From':   '+14155992671',
-		            'Body':   msg
+		            'Body':   'test'
 		        }
 		    })
 		    .success(function(data){
